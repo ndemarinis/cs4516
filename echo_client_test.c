5113cdcec6b535410fb813c49ff975e262875083
Nicholas DeMarinis <ndemarinis@wpi.edu>
2012-04-08 14:18:44 -0400

    Reverted old echo client test from bad save.

--- echo_client_test.c
+++ echo_client_test.c
@@ -23,27 +23,21 @@
 #define TERMINATOR_STR { 0x10, 0x03 } // Our termination sequence, a string of two bytes
 #define TERM_STR_LEN 2
 
-#define FILE_SIZE 2048
-
 // Prototypes
 void die_with_error(char *msg);
 
 int main(int argc, char *argv[])
 {
   int n;
-  int sock, bytes_recvd, file_len, bytes_remaining, len;
+  int sock, bytes_recvd;
   unsigned int echo_str_len;
-  char *srv_ip, *file_name;
-  FILE* fp_in, fp_out;
+  char *srv_ip, *echo_str;
   
   struct hostent *srv_host;
   struct sockaddr_in srv_addr;
 
   int pipes[2];
 
-  char file_buffer[FILE_SIZE];
-  char *f_ptr = file_buffer;
-  
   struct packet out, in;
   
   if((argc < 3) || (argc > 7))
@@ -54,7 +48,7 @@ int main(int argc, char *argv[])
     }
 
   srv_ip = argv[1];
-  file_name = argv[2];
+  echo_str = argv[2];
 
   setvbuf(stdout, NULL, _IONBF, 0);
 
@@ -76,6 +70,7 @@ int main(int argc, char *argv[])
   if((connect(sock, (struct sockaddr*)(&srv_addr), sizeof(srv_addr))) < 0)
     die_with_error("connect() failed!");
  
+  echo_str_len = strlen(argv[2]) + 1;
 
   // Make the layer stack
   if((init_layer_stack(sock, pipes)))
@@ -83,42 +78,9 @@ int main(int argc, char *argv[])
 
   sleep(1);  // Wait for the thread creation to settle.  
 
-  fp_in = fopen(file_name, "r+");
-  file_len = fsize(file_name);
-  fp_out = fopen("output.jpg", "w+");
-
-  if(read(fileno(fp), file_buffer, file_len) != file_len)
-    die_with_error("Error reading file!");
-
-  bytes_remaining = file_len;
-  while(bytes_remaining > 0)
+  for(n = 0; n < 1; n++)
     {
-      memset(&out, 0, sizeof(struct packet));
-      memset(&in, 0, sizeof(struct packet));
-
-      len = (bytes_remaining >= PACKET_PAYLOAD_SIZE) ? PACKET_PAYLOAD_SIZE : bytes_remaining;
-
-      memcpy(out.payload, f_ptr, len);
-
-      f_ptr += len;
-      bytes_remaining -= len;
-
-      out.length = len - 1;
-
-      if((write(pipe_write(pipes), &out, sizeof(struct packet)) != sizeof(struct packet)))
-	die_with_error("send() sent a different number of bytes than expected");
-      
-      if((bytes_recvd = read(pipe_read(pipes), &in, sizeof(struct packet)) <= 0))
-	 die_with_error("recv() failed or connection closed unexpectedly!");      
-
-      write(fileno(fp), in.payload, out.length);
-    }
-
-  fclose(f_in);
-  fflush(f_out);
-  fclose(f_out);
-#if 0
-  printf("\n\nSending a new string of length %d bytes...\n", echo_str_len);
+      printf("\n\nSending a new string of length %d bytes...\n", echo_str_len);
       if(echo_str_len > PACKET_PAYLOAD_SIZE)
 	die_with_error("String too large for one packet!");
 
@@ -135,10 +97,12 @@ int main(int argc, char *argv[])
 	 die_with_error("recv() failed or connection closed unexpectedly!");
 
       printf("Received string of %d bytes:  %s\n", in.length + 1, in.payload);
-#endif
     }
 
   // Cleanup
+  printf("Got all strings, waiting 2 seconds before terminating...\n");
+  sleep(2);
+
   printf("Echo Client:  Done.\n"); 
   close(sock);
   exit(0);
